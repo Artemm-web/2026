5e9ac69b7685052aede7521d7942e98c5722558f
Артем Черний <infinity.artemm@gmail.com>
2026-09-10 23:16:49 +0300

    Add files via upload
    Laba 1

---
+++ Laba-1.py
@@ -0,0 +1,24 @@
+'''
+Лабораторна №1
+'''
+
+print("Hello, World!")
+
+a = 10
+b = 5
+name = "Student"
+number = 4.7
+boolean = True
+
+print(a, type(a))
+print(b, type(b))
+print(name, type(name))
+print(number, type(number))
+print(boolean, type(boolean))
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
